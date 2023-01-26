ba40de56becbbcf5afb0fc012df26d97ec712259
Antony <antony.budianto@tokopedia.com>
2023-01-26 11:18:56 +0700

    init swc remove export const

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
+use swc_core::common::util::take::Take;
+use swc_core::ecma::ast::Pat::Ident;
+use swc_core::ecma::ast::{Decl, ModuleItem, Stmt};
 use swc_core::ecma::{
-    ast::{Ident, Program},
+    ast::{Program, VarDeclarator},
     transforms::testing::test,
     visit::{as_folder, FoldWith, VisitMut, VisitMutWith},
 };
-
 use swc_core::plugin::{plugin_transform, proxies::TransformPluginProgramMetadata};
 
 pub struct TransformVisitor;
@@ -13,20 +15,102 @@ impl VisitMut for TransformVisitor {
     // A comprehensive list of possible visitor methods can be found here:
     // https://rustdoc.swc.rs/swc_ecma_visit/trait.VisitMut.html
 
-    // fn visit_mut_bin_expr(&mut self, e: &mut BinExpr) {
-    //     e.visit_mut_children_with(self);
+    fn visit_mut_export_decl(&mut self, v: &mut swc_core::ecma::ast::ExportDecl) {
+        v.visit_mut_children_with(self);
 
-    //     if e.op == op!("===") {
-    //         e.left = Box::new(Ident::new("kdy1".into(), e.left.span()).into());
-    //     }
-    // }
+        // v.decl.take();
 
-    fn visit_mut_ident(&mut self, n: &mut Ident) {
-        n.visit_mut_children_with(self);
+        // match &v.decl {
+        //     Decl::Var(vd) => {
+        //         match &vd.decls[0].name {
+        //             Ident(i) => {
+        //                 println!("LOG:{}", &*i.sym);
+        //                 if &*i.sym == "getServerSideProps" {
+        //                     println!(">> removed!");
+        //                     v.span.take();
+        //                     // v.decl.take();
+        //                 }
+        //             }
+        //             _ => {}
+        //         }
+        //     }
+        //     _ => {}
+        // }
+    }
 
-        if n.sym.to_string() == "__DEV__" {
-            n.sym = "false".into();
+    fn visit_mut_var_declarator(&mut self, v: &mut VarDeclarator) {
+        // This is not required in this example, but you typically need this.
+        v.visit_mut_children_with(self);
+
+        // v.name is `Pat`.
+        // See https://rustdoc.swc.rs/swc_ecma_ast/enum.Pat.html
+        match &v.name {
+            // If we want to delete the node, we should return false.
+            //
+            // Note the `&*` before i.sym.
+            // The type of symbol is `JsWord`, which is an interned string.
+            Ident(i) => {
+                if &*i.sym == "getServerSideProps" {
+                    // Take::take() is a helper function, which stores invalid value in the node.
+                    // For Pat, it's `Pat::Invalid`.
+                    v.name.take();
+                }
+            }
+            _ => {
+                // Noop if we don't want to delete the node.
+            }
         }
+    }
+
+    fn visit_mut_var_declarators(&mut self, vars: &mut Vec<VarDeclarator>) {
+        vars.visit_mut_children_with(self);
+
+        vars.retain(|node| {
+            // We want to remove the node, so we should return false.
+            if node.name.is_invalid() {
+                return false;
+            }
+
+            // Return true if we want to keep the node.
+            true
+        });
+    }
+
+    fn visit_mut_stmt(&mut self, s: &mut Stmt) {
+        s.visit_mut_children_with(self);
+
+        match s {
+            Stmt::Decl(Decl::Var(var)) => {
+                if var.decls.is_empty() {
+                    // Variable declaration without declarator is invalid.
+                    //
+                    // After this, `s` becomes `Stmt::Empty`.
+                    s.take();
+                }
+            }
+            _ => {}
+        }
+    }
+
+    fn visit_mut_stmts(&mut self, stmts: &mut Vec<Stmt>) {
+        stmts.visit_mut_children_with(self);
+
+        // We remove `Stmt::Empty` from the statement list.
+        // This is optional, but it's required if you don't want extra `;` in output.
+        stmts.retain(|s| {
+            // We use `matches` macro as this match is trivial.
+            !matches!(s, Stmt::Empty(..))
+        });
+    }
+
+    fn visit_mut_module_items(&mut self, stmts: &mut Vec<ModuleItem>) {
+        stmts.visit_mut_children_with(self);
+
+        // This is also required, because top-level statements are stored in `Vec<ModuleItem>`.
+        stmts.retain(|s| {
+            // We use `matches` macro as this match is trivial.
+            !matches!(s, ModuleItem::Stmt(Stmt::Empty(..)))
+        });
     }
 }
 
@@ -66,6 +150,21 @@ test!(
     Default::default(),
     |_| as_folder(TransformVisitor),
     simple_transform_global_var,
-    r#"let isDev = __DEV__;"#,
-    r#"let isDev = false;"#
+    r#"
+export const getServerSideProps = async context => {
+    return {
+        props: {
+            a: 1,
+        },
+    };
+};
+const Home = () => {
+    return null;
+};
+export default Home;"#,
+    r#"
+const Home = () => {
+    return null;
+};
+export default Home;"#
 );
