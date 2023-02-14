8f4c287e5d17ed6bf9f510101da06e9bab23aa6a
Antony <antony.budianto@tokopedia.com>
2023-02-14 14:53:43 +0700

    feat: use export decl visitor and add toolchain file

--- src/lib.rs
+++ src/lib.rs
@@ -20,33 +20,53 @@ impl VisitMut for TransformVisitor {
     // A comprehensive list of possible visitor methods can be found here:
     // https://rustdoc.swc.rs/swc_ecma_visit/trait.VisitMut.html
 
-    fn visit_mut_var_declarator(&mut self, v: &mut VarDeclarator) {
-        // This is not required in this example, but you typically need this.
-        v.visit_mut_children_with(self);
-
-        // v.name is `Pat`.
-        // See https://rustdoc.swc.rs/swc_ecma_ast/enum.Pat.html
-        match &v.name {
-            // If we want to delete the node, we should return false.
-            //
-            // Note the `&*` before i.sym.
-            // The type of symbol is `JsWord`, which is an interned string.
-            Ident(i) => {
-                if &*i.sym == EXPORT_NAME_GET_SERVER_PROPS
-                    || &*i.sym == EXPORT_NAME_GET_INITIAL_PROPS
-                    || &*i.sym == EXPORT_NAME_GET_STATIC_CONFIG
-                    || &*i.sym == EXPORT_NAME_GET_STATIC_PROPS
-                {
-                    // Take::take() is a helper function, which stores invalid value in the node.
-                    // For Pat, it's `Pat::Invalid`.
-                    v.name.take();
+    fn visit_mut_export_decl(&mut self, v: &mut swc_core::ecma::ast::ExportDecl) {
+        match &v.decl {
+            Decl::Var(vd) => match &vd.decls[0].name {
+                Ident(i) => {
+                    if &*i.sym == EXPORT_NAME_GET_SERVER_PROPS
+                        || &*i.sym == EXPORT_NAME_GET_INITIAL_PROPS
+                        || &*i.sym == EXPORT_NAME_GET_STATIC_CONFIG
+                        || &*i.sym == EXPORT_NAME_GET_STATIC_PROPS
+                    {
+                        v.decl.take();
+                    }
                 }
-            }
-            _ => {
-                // Noop if we don't want to delete the node.
-            }
+                _ => {}
+            },
+            _ => {}
         }
     }
+
+    /**
+     * We can also use this, but this one will also deletes non-export variables/fn
+     */
+    // fn visit_mut_var_declarator(&mut self, v: &mut VarDeclarator) {
+    //     v.visit_mut_children_with(self);
+
+    //     // v.name is `Pat`.
+    //     // See https://rustdoc.swc.rs/swc_ecma_ast/enum.Pat.html
+    //     match &v.name {
+    //         // If we want to delete the node, we should return false.
+    //         //
+    //         // Note the `&*` before i.sym.
+    //         // The type of symbol is `JsWord`, which is an interned string.
+    //         Ident(i) => {
+    //             if &*i.sym == EXPORT_NAME_GET_SERVER_PROPS
+    //                 || &*i.sym == EXPORT_NAME_GET_INITIAL_PROPS
+    //                 || &*i.sym == EXPORT_NAME_GET_STATIC_CONFIG
+    //                 || &*i.sym == EXPORT_NAME_GET_STATIC_PROPS
+    //             {
+    //                 // Take::take() is a helper function, which stores invalid value in the node.
+    //                 // For Pat, it's `Pat::Invalid`.
+    //                 v.name.take();
+    //             }
+    //         }
+    //         _ => {
+    //             // Noop if we don't want to delete the node.
+    //         }
+    //     }
+    // }
 
     fn visit_mut_var_declarators(&mut self, vars: &mut Vec<VarDeclarator>) {
         vars.visit_mut_children_with(self);
@@ -107,18 +127,6 @@ pub fn process_transform(program: Program, _metadata: TransformPluginProgramMeta
     program.fold_with(&mut as_folder(TransformVisitor))
 }
 
-// An example to test plugin transform.
-// Recommended strategy to test plugin's transform is verify
-// the Visitor's behavior, instead of trying to run `process_transform` with mocks
-// unless explicitly required to do so.
-// test!(
-//     Default::default(),
-//     |_| as_folder(TransformVisitor),
-//     simple_transform_kdy1,
-//     r#"foo === bar;"#,
-//     r#"kdy1 === bar;"#
-// );
-
 test!(
     Default::default(),
     |_| as_folder(TransformVisitor),
@@ -136,6 +144,36 @@ const Home = () => {
 };
 export default Home;"#,
     r#"
+const Home = () => {
+    return null;
+};
+export default Home;"#
+);
+
+test!(
+    Default::default(),
+    |_| as_folder(TransformVisitor),
+    not_delete_non_export_getserversideprops,
+    r#"
+const getServerSideProps = async context => {
+    return {
+        props: {
+            a: 1,
+        },
+    };
+};
+const Home = () => {
+    return null;
+};
+export default Home;"#,
+    r#"
+const getServerSideProps = async context => {
+    return {
+        props: {
+            a: 1,
+        },
+    };
+};
 const Home = () => {
     return null;
 };
